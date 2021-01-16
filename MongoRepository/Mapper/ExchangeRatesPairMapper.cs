ba41ab203001708587c8b5f2d3dab69baee59b2c
rafielharar <rafielharar@gmail.com>
2021-01-17 00:20:09 +0200

    Add files via upload

---
+++ MongoRepository/Mapper/ExchangeRatesPairMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Common;
+using MongoRepository.Model;
+
+namespace MongoRepository.Mapper
+{
+    public class ExchangeRatesPairMapper:Profile
+    {
+        public ExchangeRatesPairMapper()
+        {
+            CreateMap<ExchangeRatesPair, ExchangeRatesPairForMongo>();
+            CreateMap<ExchangeRatesPairForMongo, ExchangeRatesPair>();
+        }
+    }
+}
---
+++ MongoRepository/Model/ExchangeRatesPairForMongo.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace MongoRepository.Model
+{
+    [BsonIgnoreExtraElements]
+    public class ExchangeRatesPairForMongo: ExchangeRatesPair
+    {
+    }
+}
